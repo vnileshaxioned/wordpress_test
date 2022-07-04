5de5cd0e8ddb4ebc102351a46243aab6ba01d3b9
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-04 12:18:46 +0530

    added header and footer

---
+++ wp-content/themes/axionedtheme/footer.php
@@ -0,0 +1,10 @@
+<!--footer section start-->
+    <footer>      
+      <?php wp_footer(); ?>
+    </footer>
+    <!--footer section end-->
+
+  </div>
+  <!--container end-->
+</body>
+</html>
---
+++ wp-content/themes/axionedtheme/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<!-- If multi-language site, reconsider usage of html lang declaration here. -->
+<html lang="en"> 
+
+<head>
+  <meta charset="utf-8">
+  <?php wp_head(); ?>
+</head>
+<body>
+  <!--container start-->
+  <div class="container">
+    <!--header section start-->
+    <header>
+      
+    </header>
+    <!--header section start-->
