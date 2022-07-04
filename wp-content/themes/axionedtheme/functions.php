e30f687530a5dd968fcaf97dc97430679ff59612
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-04 13:49:33 +0530

    added navbar

--- wp-content/themes/axionedtheme/functions.php
+++ wp-content/themes/axionedtheme/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+add_action('wp_enqueue_scripts', 'test_theme_script');
+function test_theme_script() {
+  wp_enqueue_style('custom-styling', get_stylesheet_uri());
+}
+
+add_action('after_setup_theme', 'test_theme_setup');
+function test_theme_setup() {
+  add_theme_support('title-tag');
+  add_theme_support( 'post-thumbnails' );
+  register_nav_menus( array(
+    'primary' => __('Primary Menu', 'axionedtheme')
+  ) );
+}
 
 add_filter('use_block_editor_for_post', '__return_false');
 
--- wp-content/themes/axionedtheme/header.php
+++ wp-content/themes/axionedtheme/header.php
@@ -12,6 +12,15 @@
   <div class="container">
     <!--header section start-->
     <header>
-      
+      <div class="wrapper">
+        <h1><a href="<?php bloginfo('url') ?>" class="axioned-logo" title="Axioned logo"><img src="https://dummyimage.com/159x58/000/fff.jpg" alt="Axioned Logo">Axioned logo</a></h1>
+        <?php wp_nav_menu( array(
+          'theme_location' => 'primary',
+          'menu_class' => 'navbar',
+          'container' => 'nav',
+          'container_class' => 'menu',
+          'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+        ) ); ?>
+      </div>
     </header>
     <!--header section start-->
