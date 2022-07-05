5ef83495c82333a96bf222bf28e8a6c7ec981b2e
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-05 12:32:45 +0530

    works template updated

--- wp-content/themes/axionedtheme/works.php
+++ wp-content/themes/axionedtheme/works.php
@@ -5,9 +5,10 @@
 ?>
 <main>
   <?php
-    if (have_rows('home_page')) {
-      while (have_rows('home_page')) {
+    if (have_rows('work_page')) {
+      while (have_rows('work_page')) {
         the_row();
+        echo 'yes';
         ?>
           <?php get_template_part('template-parts/pages/work/content', 'work'); ?>
           <?php get_template_part('template-parts/pages/work/content', 'slider'); ?>
