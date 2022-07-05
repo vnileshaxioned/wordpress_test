204c855af34b952a1aa5f16360a70cebdea47388
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-05 12:09:51 +0530

    added work template

--- wp-content/themes/axionedtheme/functions.php
+++ wp-content/themes/axionedtheme/functions.php
@@ -16,6 +16,43 @@ function test_theme_setup() {
   ) );
 }
 
+add_action( 'init', 'work_post_type');
+function work_post_type() {
+  $labels = array(
+    'name'                => _x( 'Work', 'Post Type General Name', 'axionedtheme' ),
+    'singular_name'       => _x( 'Work', 'Post Type Singular Name', 'axionedtheme' ),
+    'menu_name'           => __( 'Work', 'axionedtheme' ),
+    'all_items'           => __( 'All Work', 'axionedtheme' ),
+    'add_new'             => __( 'Add New', 'axionedtheme' ),
+    'edit_item'           => __( 'Edit Work', 'axionedtheme' ),
+    'update_item'         => __( 'Update Work', 'axionedtheme' ),
+    'search_items'        => __( 'Search Work', 'axionedtheme' ),
+    'not_found'           => __( 'Not Found', 'axionedtheme' ),
+    'not_found_in_trash'  => __( 'Not found in Trash', 'axionedtheme' ),
+  );
+  $args = array(
+    'label'               => __( 'work', 'axionedtheme' ),
+    'description'         => __( 'work description', 'axionedtheme' ),
+    'labels'              => $labels,
+    'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail' ),
+    'hierarchical'        => true,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 4,
+    'menu_icon'           => 'dashicons-portfolio',
+    'can_export'          => true,
+    'has_archive'         => true,
+    'exclude_from_search' => false,
+    'publicly_queryable'  => true,
+    'capability_type'     => 'page',
+    'taxonomies'          => array('post_tag' ),
+  );
+  register_post_type( 'work', $args );
+}
+
 add_filter('use_block_editor_for_post', '__return_false');
 
 ?>
---
+++ wp-content/themes/axionedtheme/template-parts/pages/work/content-slider.php
@@ -0,0 +1,22 @@
+<section class="clients">
+  <div class="wrapper work-wrapper">
+    <ul class="client-slider">
+      <li class="list">
+        <figure><img src="https://dummyimage.com/247x125/000/fff.jpg" alt="Image"></figure>
+      </li>
+      <li class="list">
+        <figure><img src="https://dummyimage.com/247x125/000/fff.jpg" alt="Image"></figure>
+      </li>
+      <li class="list">
+        <figure><img src="https://dummyimage.com/247x125/000/fff.jpg" alt="Image"></figure>
+      </li>
+      <li class="list">
+        <figure><img src="https://dummyimage.com/247x125/000/fff.jpg" alt="Image"></figure>
+      </li>
+    </ul>
+    <ul class="slider-action">
+      <li class="sliderp-button"><a href="#FIXME" class="previous-button" title="Previous">Previous</a></li>
+      <li class="sliderp-button"><a href="#FIXME" class="next-button" title="Next">Next</a></li>
+    </ul>
+  </div>
+</section>
