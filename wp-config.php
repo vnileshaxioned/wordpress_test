43e937e5d78199b4e19a91d593c9c2e81c5a763b
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-06 11:31:53 +0530

    updated the work templates

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'test_wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'phpmyadmin' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
--- wp-content/themes/axionedtheme/template-parts/pages/home/content-work.php
+++ wp-content/themes/axionedtheme/template-parts/pages/home/content-work.php
@@ -1,20 +1,27 @@
 <?php
   if (get_row_layout() == 'work_section') {
     $works = get_sub_field('work');
-    ?>
-      <section class="work">
-        <div class="wrapper home-wrapper">
-          <ul class="outter-box">
-            <?php foreach ($works as $work) { ?>
-              <li class="inner-box">
-                <a href="#FIXME" class="work-link"><img src="<?php echo $work['image']['url']; ?>" class="work-image" alt="<?php echo $work['image']['alt']; ?>"></a>
-                <div class="content">
-                  <h3 class="work-heading"><?php echo $work['title'] ?></h3>
-                  <p class="work-paragraph"><?php echo $work['short_description'] ?></p>
-                </div>
-              </li>
-            <?php } ?>
-          </ul>
-        </div>
-      </section>
-    <?php } ?>
+  ?>
+  <section class="work">
+    <div class="wrapper home-wrapper">
+      <ul class="outter-box">
+        <?php
+          foreach ($works as $work) {
+            $work_content = $work['work_content'];
+            $post_id = $work_content->ID;
+            $title = $work_content->post_title;
+            $excerpt = $work_content->post_excerpt;
+            $image = get_field('image', $post_id);
+          ?>
+          <li class="inner-box">
+            <a href="#FIXME" class="work-link"><img src="<?php echo $image['url']; ?>" class="work-image" alt="<?php echo $image['alt']; ?>"></a>
+            <div class="content">
+              <h3 class="work-heading"><?php echo $title; ?></h3>
+              <p class="work-paragraph"><?php echo $excerpt; ?></p>
+            </div>
+          </li>
+        <?php } ?>
+      </ul>
+    </div>
+  </section>
+<?php } ?>
