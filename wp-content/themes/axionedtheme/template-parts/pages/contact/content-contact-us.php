f803f749ff68cb9f5e06e5a42d308418f6214ddf
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-06 12:16:10 +0530

    updated contact us template part

--- wp-content/themes/axionedtheme/template-parts/pages/contact/content-contact-us.php
+++ wp-content/themes/axionedtheme/template-parts/pages/contact/content-contact-us.php
@@ -1,38 +1,31 @@
-<?php if (get_row_layout() == 'contact_us_section') { ?>
+<?php
+  if (get_row_layout() == 'contact_us_section') {
+    $title = get_sub_field('section_title');
+    $contents = get_sub_field('add_content');
+  ?>
   <section class="contact-us">
     <div class="wrapper section-wrapper">
       <h2 class="contact-us-heading">Contact Us</h2>
       <ul class="contact-outter-box">
-        <li class="contact-list">
-          <figure>
-            <img src="https://dummyimage.com/370x140/000/fff.jpg" alt="image">
-          </figure>
-          <div class="contact-content">
-            <h3 class="contact-head">Greater NY & USA.</h3>
-            <p class="contact-address">833 Broadway New York, NY</p>
-            <p class="tel-number"><a href="tel:+1 917 275 7105" title="+1 917 275 7105">T: +1 917 275 7105</a></p>
-          </div>
-        </li>
-        <li class="contact-list">
-          <figure>
-            <img src="https://dummyimage.com/370x140/000/fff.jpg" alt="image">
-          </figure>
-          <div class="contact-content">
-            <h3 class="contact-head">Greater NY & USA.</h3>
-            <p class="contact-address">833 Broadway New York, NY</p>
-            <p class="tel-number"><a href="tel:+1 917 275 7105" title="+1 917 275 7105">T: +1 917 275 7105</a></p>
-          </div>
-        </li>
-        <li class="contact-list">
-          <figure>
-            <img src="https://dummyimage.com/370x140/000/fff.jpg" alt="image">
-          </figure>
-          <div class="contact-content">
-            <h3 class="contact-head">Greater NY & USA.</h3>
-            <p class="contact-address">833 Broadway New York, NY</p>
-            <p class="tel-number"><a href="tel:+1 917 275 7105" title="+1 917 275 7105">T: +1 917 275 7105</a></p>
-          </div>
-        </li>
+        <?php
+          foreach ($contents as $content) {
+            $image = $content['image']['url'];
+            $image_alt = $content['image']['alt'];
+            $heading = $content['title'];
+            $address = $content['address'];
+            $contact_number = $content['contact_number'];
+          ?>
+          <li class="contact-list">
+            <figure>
+              <img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
+            </figure>
+            <div class="contact-content">
+              <h3 class="contact-head"><?php echo $heading; ?></h3>
+              <p class="contact-address"><?php echo $address; ?></p>
+              <p class="tel-number"><a href="tel:+1 917 275 7105" title="<?php echo $contact_number; ?>"><?php echo $contact_number; ?></a></p>
+            </div>
+          </li>
+        <?php } ?>
       </ul>
     </div>
   </section>
