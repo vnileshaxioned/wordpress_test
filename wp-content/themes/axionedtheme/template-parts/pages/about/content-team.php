29a7906b43bd4697b11affc3fb5461f00c57f8b0
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-07 14:03:02 +0530

    added about page template parts files

---
+++ wp-content/themes/axionedtheme/template-parts/pages/about/content-team.php
@@ -0,0 +1,49 @@
+<section class="team">
+  <div class="wrapper inner-wrapper">
+    <figure class="section-background-image">
+      <img src="https://dummyimage.com/1247x572/7A7873/fff.jpg" alt="image">
+    </figure>
+		<h3 class="about-heading">Our Remote & Global Team</h3>
+    <ul class="team-content">
+			<li class="team-list">
+				<figure>
+					<img src="https://dummyimage.com/48x48/000/fff.jpg" alt="Image">
+				</figure>
+				<h4 class="team-heading">Greater-NY <span>(EST)</span></h4>
+				<ul class="team-list-content">
+					<li class="team-inner-list">UX/UI Design</li>
+					<li class="team-inner-list">Project Management</li>
+					<li class="team-inner-list">Digital Transformation</li>
+					<li class="team-inner-list">Front-End Dev</li>
+					<li class="team-inner-list">Back-End Dev</li>
+				</ul>
+			</li>
+			<li class="team-list">
+				<figure>
+					<img src="https://dummyimage.com/48x48/000/fff.jpg" alt="Image">
+				</figure>
+				<h4 class="team-heading">Greater-NY <span>(EST)</span></h4>
+				<ul class="team-list-content">
+					<li class="team-inner-list">UX/UI Design</li>
+					<li class="team-inner-list">Project Management</li>
+					<li class="team-inner-list">Digital Transformation</li>
+					<li class="team-inner-list">Front-End Dev</li>
+					<li class="team-inner-list">Back-End Dev</li>
+				</ul>
+			</li>
+			<li class="team-list">
+				<figure>
+					<img src="https://dummyimage.com/48x48/000/fff.jpg" alt="Image">
+				</figure>
+				<h4 class="team-heading">Greater-NY <span>(EST)</span></h4>
+				<ul class="team-list-content">
+					<li class="team-inner-list">UX/UI Design</li>
+					<li class="team-inner-list">Project Management</li>
+					<li class="team-inner-list">Digital Transformation</li>
+					<li class="team-inner-list">Front-End Dev</li>
+					<li class="team-inner-list">Back-End Dev</li>
+				</ul>
+			</li>
+		</ul>
+  </div>
+</section>
