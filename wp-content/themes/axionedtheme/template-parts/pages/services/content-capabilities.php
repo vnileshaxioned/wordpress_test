68290200d520c3e5287830e87a17fd317d3e18a7
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-07 12:19:59 +0530

    update service with dynamic content

--- wp-content/themes/axionedtheme/template-parts/pages/services/content-capabilities.php
+++ wp-content/themes/axionedtheme/template-parts/pages/services/content-capabilities.php
@@ -1,26 +1,42 @@
-<?php if (get_row_layout() == 'services_and_capabilities_section') { ?>
+<?php
+  if (get_row_layout() == 'services_and_capabilities_section') {
+    $title = get_sub_field('section_title');
+    $contents = get_sub_field('service_content');
+  ?>
   <section class="capabilities">
     <div class="wrapper services-wrapper">
-      <h2 class="service-heading">Services & Capabilities</h2>
+      <h2 class="service-heading"><?php echo $title; ?></h2>
       <ul class="service-outter-box">
-        <li class="service-list">
-          <figure class="service-img">
-            <img src="https://dummyimage.com/468x234/000/fff.jpg" alt="Image">
-          </figure>
-          <div class="service-content">
-            <h3 class="service-title">Digital Experiences</h3>
-            <p class="service-paragraph">Axioned supports the launch and ongoing enhnacement & maintenance of digital user experiences designed to optimize brand reputation and support lead geaneration/CTRs</p>
-          </div>
-        </li>
-        <li class="service-list">
-          <div class="service-content">
-            <h3 class="service-title">Digital Experiences</h3>
-            <p class="service-paragraph">Axioned supports the launch and ongoing enhnacement & maintenance of digital user experiences designed to optimize brand reputation and support lead geaneration/CTRs</p>
-          </div>
-          <figure class="service-img">
-            <img src="https://dummyimage.com/468x234/000/fff.jpg" alt="Image">
-          </figure>
-        </li>
+        <?php
+          foreach ($contents as $content) {
+            $image = $content['content_image']['url'];
+            $image_alt = $content['content_image']['alt'];
+            $heading = $content['content_title'];
+            $paragraph = $content['content_paragraph'];
+            $image_side = $content['image_side'];
+
+            if ($image_side == 'Left') {
+          ?>
+          <li class="service-list">
+            <figure class="service-img">
+              <img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
+            </figure>
+            <div class="service-content">
+              <h3 class="service-title"><?php echo $heading; ?></h3>
+              <p class="service-paragraph"><?php echo $paragraph; ?></p>
+            </div>
+          </li>
+          <?php } else { ?>
+          <li class="service-list">
+            <div class="service-content">
+              <h3 class="service-title"><?php echo $heading; ?></h3>
+              <p class="service-paragraph"><?php echo $paragraph; ?></p>
+            </div>
+            <figure class="service-img">
+              <img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>">
+            </figure>
+          </li>
+        <?php } } ?>
       </ul>
     </div>
   </section>
--- wp-content/themes/axionedtheme/template-parts/pages/services/content-technologies.php
+++ wp-content/themes/axionedtheme/template-parts/pages/services/content-technologies.php
@@ -1,32 +1,33 @@
-<?php if (get_row_layout() == 'tools_and_technologies_section') { ?>
+<?php
+  if (get_row_layout() == 'tools_and_technologies_section') {
+    $title = get_sub_field('section_title');
+    $contents = get_sub_field('section_content');
+  ?>
   <section class="tools">
     <div class="wrapper services-wrapper">
-      <h2 class="tools-heading">Team Axioned: Remote, Global Coverage</h2>
+      <h2 class="tools-heading"><?php echo $title; ?></h2>
       <ul class="tools-outter-box">
-        <li class="tools-list"><a href="#FIXME" class="list-name" data-tab-name="All" title="All">All</a></li>
-        <li class="tools-list"><a href="#FIXME" class="list-name" data-tab-name="Content" title="Content">Content</a></li>
-        <li class="tools-list"><a href="#FIXME" class="list-name" data-tab-name="Digital experiences" title="Digital experiences">Digital experiences</a></li>
-        <li class="tools-list"><a href="#FIXME" class="list-name" data-tab-name="Data" title="Data">Data</a></li>
+        <?php
+          foreach ($contents as $content) {
+            $tab_title = $content['tab_title'];
+            $tab_images = $content['tab_images'];
+          ?>
+          <li class="tools-list"><a href="#FIXME" class="list-name" data-tab-name="<?php echo $tab_title; ?>" title="<?php echo $tab_title; ?>"><?php echo $tab_title; ?></a></li>
+        <?php } ?>
       </ul>
       <ul class="tools-image-box">
-        <li class="tools-image-list" data-list-name="Content">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
-        <li class="tools-image-list" data-list-name="Data">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
-        <li class="tools-image-list" data-list-name="Digital experiences">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
-        <li class="tools-image-list" data-list-name="Content">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
-        <li class="tools-image-list" data-list-name="Digital experiences">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
-        <li class="tools-image-list" data-list-name="Data">
-          <figure class="image-list"><img src="https://dummyimage.com/54x54/000/fff.jpg" alt="Image"></figure>
-        </li>
+        <?php
+          foreach ($contents as $content) {
+            $tab_title = $content['tab_title'];
+            $tab_images = $content['tab_images'];
+            foreach ($tab_images as $tab_image) {
+            $image = $tab_image['image']['url'];
+            $image_alt = $tab_image['image']['alt'];
+          ?>
+          <li class="tools-image-list" data-list-name="<?php echo $tab_title; ?>">
+            <figure class="image-list"><img src="<?php echo $image; ?>" alt="<?php echo $image_alt; ?>"></figure>
+          </li>
+        <?php } } ?>
       </ul>
     </div>
   </section>
