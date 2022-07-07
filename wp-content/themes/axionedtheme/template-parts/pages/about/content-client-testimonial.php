5c553146dc9fffddb1dc6330813c649406737539
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-07 15:37:17 +0530

    update about team template part

--- wp-content/themes/axionedtheme/template-parts/pages/about/content-client-testimonial.php
+++ wp-content/themes/axionedtheme/template-parts/pages/about/content-client-testimonial.php
@@ -1,25 +1,30 @@
 <section class="client-testimonials">
     <div class="wrapper home-wrapper">
-      <h4 class="client-title">Client testimonials</h4>
-      <ul class="client-outter-box">
-        <li class="client-inner-box">
-          <figure>
-            <img src="https://dummyimage.com/200x55/000/fff.jpg" class="client-image" alt="Image">
-          </figure>
-          <div class="content">
-            <h5 class="client-heading">Shutterstock</h5>
-            <p class="client-paragraph">"The Axioned team fully met our expectations. They were proactive, very patient, communicative & organized in terms of the deliverables and timeline. They also accommodated our last-minute changes. The overall process working with the team was smooth, enjoyable & GREAT! You guys consistently do a great job!"</p>
-          </div>
-        </li>
-        <li class="client-inner-box">
-          <figure>
-            <img src="https://dummyimage.com/200x55/000/fff.jpg" class="client-image" alt="Image">
-          </figure>
-          <div class="content">
-            <h5 class="client-heading">Pioneer Academics</h5>
-            <p class="client-paragraph">"The team is great to work with. They are diligent, knowledegeable, attentive to detail & are always open to feedback. We appreciate their dedication. They make sure that tasks are completed on time and are quick in understanding client requirements. Axioned and the team are great. We enjoy working with them!"</p>
-          </div>
-        </li>
-      </ul>
+      <figure class="section-background-image">
+        <img src="https://dummyimage.com/1247x572/000/fff.jpg" alt="image">
+      </figure>
+      <div class="about-content">
+        <h4 class="client-title">Client testimonials</h4>
+        <ul class="client-outter-box">
+          <li class="client-inner-box">
+            <figure>
+              <img src="https://dummyimage.com/200x55/000/fff.jpg" class="client-image" alt="Image">
+            </figure>
+            <div class="content">
+              <h5 class="client-heading">Shutterstock</h5>
+              <p class="client-paragraph">"The Axioned team fully met our expectations. They were proactive, very patient, communicative & organized in terms of the deliverables and timeline. They also accommodated our last-minute changes. The overall process working with the team was smooth, enjoyable & GREAT! You guys consistently do a great job!"</p>
+            </div>
+          </li>
+          <li class="client-inner-box">
+            <figure>
+              <img src="https://dummyimage.com/200x55/000/fff.jpg" class="client-image" alt="Image">
+            </figure>
+            <div class="content">
+              <h5 class="client-heading">Pioneer Academics</h5>
+              <p class="client-paragraph">"The team is great to work with. They are diligent, knowledegeable, attentive to detail & are always open to feedback. We appreciate their dedication. They make sure that tasks are completed on time and are quick in understanding client requirements. Axioned and the team are great. We enjoy working with them!"</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
