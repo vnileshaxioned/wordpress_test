747eaae91bd1250a5168e5ff5aaea02c92937bd4
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-13 15:40:49 +0530

    updated functions file of child theme

--- wp-content/themes/axionedchildtheme/functions.php
+++ wp-content/themes/axionedchildtheme/functions.php
@@ -3,8 +3,8 @@
 // enqueue the style and script files
 add_action('wp_enqueue_scripts', 'test_theme_scripts');
 function test_theme_scripts() {
-  wp_enqueue_style('custom-styling', get_stylesheet_uri());
-  wp_enqueue_style('custom-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('custom-styling', get_template_directory_uri() . '/style.css',);
+  wp_enqueue_style('child-custom-styling', get_stylesheet_uri(), array('custom-styling'));
 }
 
 ?>
