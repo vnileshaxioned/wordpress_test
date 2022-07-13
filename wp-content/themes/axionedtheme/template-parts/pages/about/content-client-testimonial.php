2cb753774ef0f28a169f8ca9c53fb615e1c51966
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-13 13:48:18 +0530

    added option page to home and about page for clienty testimonial

--- wp-content/themes/axionedtheme/template-parts/pages/about/content-client-testimonial.php
+++ wp-content/themes/axionedtheme/template-parts/pages/about/content-client-testimonial.php
@@ -1,65 +1,64 @@
 <?php
-  $background_image = get_sub_field('background_image');
-  $title = get_sub_field('section_title');
-  $client_testimonials = get_sub_field('client_testimonial');
-  $image_url = $background_image['url'] ? $background_image['url'] : null;
-  $image_alt = $background_image['alt'] ? $background_image['alt'] : $title;
+  $show_section = get_sub_field('show_section');
 
-  if ($image_url || $title || $client_testimonials) {
-  ?>
-  <section class="client-testimonials">
-    <div class="wrapper inner-wrapper">
-      <?php if ($image_url) { ?>
-        <figure class="section-background-image">
-          <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
-        </figure>
-      <?php }
-        if ($title || $client_testimonials) {
-        ?>
-        <div class="client-content">
-          <?php
-            echo $title ? '<h2 class="main-heading">'. $title .'</h2>' : null;
+  if ($show_section) {
+    $background_image = get_sub_field('background_image');
+    $section_title = get_field('section_title', 'option');
+    $contents = get_field('client_testimonial', 'option');
+    $image_url = $background_image['url'] ? $background_image['url'] : null;
+    $image_alt = $background_image['alt'] ? $background_image['alt'] : $section_title;
 
-            if ($client_testimonials) {
-            ?>
-            <ul class="client-outter-box">
-              <?php 
-                foreach ($client_testimonials as $client_testimonial) {
-                  $image = $client_testimonial['image']['url'];
-                  $image_alt = $client_testimonial['image']['alt'];
-                  $title = $client_testimonial['title'];
-                  $description = $client_testimonial['description'];
+    if ($image_url || $section_title || $contents) {?>
+    <section class="client-testimonials">
+      <div class="wrapper inner-wrapper">
+        <?php if ($image_url) { ?>
+          <figure class="section-background-image">
+            <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
+          </figure>
+        <?php }
+          if ($section_title || $contents) { ?>
+          <div class="client-content">
+            <?php
+              echo $section_title ? '<h2 class="main-heading">'. $section_title .'</h2>' : null;
 
-                  if ($title || $image || $description) {
-                ?>
-                <li class="client-inner-box">
-                  <?php if ($image) { ?>
-                    <figure>
-                      <img src="<?php echo $image; ?>" class="client-image" alt="<?php echo $image_alt; ?>">
-                    </figure>
+              if ($contents) { ?>
+              <ul class="client-outter-box">
+                <?php 
+                  foreach ($contents as $content) {
+                    $title = $content['title'];
+                    $description = $content['description'];
+                    $image = $content['image']['url'] ? $content['image']['url'] : null;
+                    $image_alt = $content['image']['alt'] ? $content['image']['alt'] : $title;
+
+                    if ($title || $image || $description) { ?>
+                    <li class="client-inner-box">
+                      <?php if ($image) { ?>
+                        <figure>
+                          <img src="<?php echo $image; ?>" class="client-image" alt="<?php echo $image_alt; ?>">
+                        </figure>
+                      <?php }
+                        if ($title || $description) { ?>
+                        <div class="content">
+                          <?php 
+                            echo $title ? '<h4 class="client-heading">'. $title .'</h4>' : null;
+                            echo $description ? '<p class="client-paragraph">'. $description .'</p>' : null;
+                          ?>
+                        </div>
+                      <?php } ?>
+                    </li>
                   <?php }
-                    if ($title || $description) {
-                    ?>
-                    <div class="content">
-                      <?php 
-                        echo $title ? '<h4 class="client-heading">'. $title .'</h4>' : null;
-                        echo $description ? '<p class="client-paragraph">'. $description .'</p>' : null;
-                      ?>
-                    </div>
-                  <?php } ?>
-                </li>
-              <?php }
-                } ?>
-              <li class="client-slider-action">
-                <a href="#FIXME" class="client-slider-button previous-slide" title="Previous slide">Previous slide</a>
-              </li>
-              <li class="client-slider-action">
-                <a href="#FIXME" class="client-slider-button next-slide" title="Next slide">Next slide</a>
-              </li>
-            </ul>
+                    } ?>
+                  <li class="client-slider-action">
+                    <a href="#FIXME" class="client-slider-button previous-slide" title="Previous slide">Previous slide</a>
+                  </li>
+                  <li class="client-slider-action">
+                    <a href="#FIXME" class="client-slider-button next-slide" title="Next slide">Next slide</a>
+                  </li>
+                </ul>
+              <?php } ?>
+            </div>
           <?php } ?>
         </div>
-      <?php } ?>
-    </div>
-  </section>
-<?php } ?>
+      </section>
+    <?php }
+      } ?>
--- wp-content/themes/axionedtheme/template-parts/pages/home/content-client-testimonial.php
+++ wp-content/themes/axionedtheme/template-parts/pages/home/content-client-testimonial.php
@@ -1,47 +1,48 @@
 <?php
-  $title = get_sub_field('section_title');
-  $client_testimonials = get_sub_field('client_testimonial');
+  $show_section = get_sub_field('show_section');
 
-  if ($title || $client_testimonials) {
-  ?>
-  <section class="client-testimonials">
-    <div class="wrapper inner-wrapper">
-      <?php echo $title ? '<h2 class="main-heading">'. $title .'</h2>' : null; ?>
-      <ul class="client-outter-box">
-        <?php
-          foreach ($client_testimonials as $client_testimonial) {
-            $heading = $client_testimonial['title'];
-            $description = $client_testimonial['description'];
-            $image_url = $client_testimonial['image']['url'] ? $client_testimonial['image']['url'] : null;
-            $image_alt = $client_testimonial['image']['alt'] ? $client_testimonial['image']['url'] : $heading;
+  if ($show_section) {
+    $title = get_field('section_title', 'option');
+    $contents = get_field('client_testimonial', 'option');
 
-            if ($heading || $description || $image_url) {
-          ?>
-          <li class="client-inner-box">
-            <?php if ($image_url) { ?>
-              <figure>
-                <img src="<?php echo $image_url; ?>" class="client-image" alt="<?php echo $image_alt; ?>">
-              </figure>
+    if ($title || $contents) { ?>
+    <section class="client-testimonials">
+      <div class="wrapper inner-wrapper">
+        <?php echo $title ? '<h2 class="main-heading">'. $title .'</h2>' : null; ?>
+        <ul class="client-outter-box">
+          <?php
+            foreach ($contents as $content) {
+              $heading = $content['title'];
+              $description = $content['description'];
+              $image_url = $content['image']['url'] ? $content['image']['url'] : null;
+              $image_alt = $content['image']['alt'] ? $content['image']['alt'] : $heading;
+
+              if ($heading || $description || $image_url) { ?>
+              <li class="client-inner-box">
+                <?php if ($image_url) { ?>
+                  <figure>
+                    <img src="<?php echo $image_url; ?>" class="client-image" alt="<?php echo $image_alt; ?>">
+                  </figure>
+                <?php }
+                  if ($heading || $description) { ?>
+                  <div class="content">
+                    <?php
+                      echo $heading ? '<h5 class="client-heading">'. $heading .'</h5>' : null;
+                      echo $description ? '<p class="client-paragraph">'. $description .'</p>' : null;
+                    ?>
+                  </div>
+                <?php } ?>
+              </li>
             <?php }
-              if ($heading || $description) {
-              ?>
-              <div class="content">
-                <?php
-                  echo $heading ? '<h5 class="client-heading">'. $heading .'</h5>' : null;
-                  echo $description ? '<p class="client-paragraph">'. $description .'</p>' : null;
-                ?>
-              </div>
-            <?php } ?>
-          </li>
-        <?php }
-          } ?>
-        <li class="client-slider-action">
-          <a href="#FIXME" class="client-slider-button previous-slide" title="Previous slide">Previous slide</a>
-        </li>
-        <li class="client-slider-action">
-          <a href="#FIXME" class="client-slider-button next-slide" title="Next slide">Next slide</a>
-        </li>
-      </ul>
-    </div>
-  </section>
-<?php } ?>
+              } ?>
+            <li class="client-slider-action">
+              <a href="#FIXME" class="client-slider-button previous-slide" title="Previous slide">Previous slide</a>
+            </li>
+            <li class="client-slider-action">
+              <a href="#FIXME" class="client-slider-button next-slide" title="Next slide">Next slide</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    <?php }
+      } ?>
